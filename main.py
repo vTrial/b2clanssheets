689f953af1e11a3c8e63b52183b4979ce651ddbe
vTrial <orange75100@gmail.com>
2023-08-02 23:00:11 -0700

    add loop every half hour

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from nkapi import get_clan_counts
 from google_sheets import update_clan_counts
+import time
 
-clan_counts = get_clan_counts()
-clan_counts += [[""] * 4] * 5
+def clan_loop():
+	clan_counts = get_clan_counts()
+	clan_counts += [[""] * 4] * 5
+	update_clan_counts(clan_counts)
 
-update_clan_counts(clan_counts)
+starttime = time.monotonic()
+seconds_to_loop = 1800
+while True:
+    clan_loop()
+    time.sleep(seconds_to_loop - ((time.monotonic() - starttime) % seconds_to_loop))
