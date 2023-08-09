fa36fbfa7b7d716d99966a4b950ed976338b9383
vTrial <orange75100@gmail.com>
2023-08-09 10:06:41 -0700

    clans code no longer breaks on new season

--- nkapi.py
+++ nkapi.py
@@ -1,33 +1,39 @@
 import requests
-def get_lb_total_pages():
+def get_lb_total_pages(season):
     homs_url = "https://data.ninjakiwi.com/battles2/homs"
     homs_data = requests.request("GET", homs_url).json()
-    total_hom_players = homs_data['body'][0]["totalScores"]
+    season_index = [hom_data['name'].split(" ")[1] for hom_data in homs_data["body"]].index(str(season))
+    total_hom_players = homs_data['body'][season_index]["totalScores"]
     lb_page_count = (total_hom_players - 1) // 50 + 1
     return lb_page_count
 
-def get_player_list():
+def get_player_list(season):
     player_list = []
-    lb_total_pages = get_lb_total_pages()
+    lb_total_pages = get_lb_total_pages(season)
     for lb_page_num in range(1, lb_total_pages + 1):
-        lb_url = f"https://data.ninjakiwi.com/battles2/homs/season_12/leaderboard?page={lb_page_num}"
+        lb_url = f"https://data.ninjakiwi.com/battles2/homs/season_{str(season - 1)}/leaderboard?page={lb_page_num}"
         lb_json = requests.request("GET", lb_url).json()
         player_list += [[player["displayName"], player["score"]] for player in lb_json["body"]]
     return player_list
 
-def get_filtered_player_list():
-    player_list = get_player_list()
+def get_filtered_player_list(season):
+    player_list = get_player_list(season)
     filtered_player_list = [player for player in player_list if '<' in player[0] and '>' in player[0]]
     return filtered_player_list
 
 def get_clan_counts():
-    filtered_player_list = get_filtered_player_list()
+    # update this every season
+    season = 13
+    filtered_player_list = get_filtered_player_list(season)
     clan_counts = {}
     for entry in filtered_player_list:
         name = entry[0]
         if '<' in name and '>' in name:
             start = name.index('<')
-            end = name.index('>', start)
+            try:
+                end = name.index('>', start)
+            except ValueError:
+                continue
             clan = name[start + 1:end].lower()
             clan_members = clan_counts.get(clan, [0, 0])[0] + 1
             clan_total_score = clan_counts.get(clan, [0, 0])[1] + entry[1]
@@ -36,5 +42,4 @@ def get_clan_counts():
     # secondary sort: score
     sorted_count = sorted(lst_count, key=lambda x: x[2], reverse=True)
     # primary sort: member count
-    sorted_count = sorted(sorted_count, key=lambda x: x[1], reverse=True)
     return sorted_count
